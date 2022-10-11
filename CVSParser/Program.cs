f526b90911ae646dab76276c2120df6a16bf2ec5
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2022-10-11 12:57:02 -0500

    Conversion Practically Done

--- CVSParser/Program.cs
+++ CVSParser/Program.cs
@@ -18,58 +18,179 @@ class CVSParser
     };
 
 
-    static public string weekdayConverter(string shorthand) {
-        if (shorthand.Length == 1){
+    static public List<Tuple<int, int>> getAvailability(Dictionary<string, Dictionary<string, List<Tuple<Tuple<int, int>, string>>>> timeBlockings, string room, string day) {
+        int earliest = 900;
+        int lateset = 2200;
+        if (!timeBlockings.ContainsKey(room)) {
+            Console.WriteLine("The room " + room + " does not exist, or perhaps it is not registered . . .");
+            return null;
+        }
+        if (!timeBlockings[room].ContainsKey(day)) {
+            Console.WriteLine(day + " is not a vaild day . . .");
+            return null;
+        }
+        List<Tuple<int, int>> result = new List<Tuple<int, int>>();
+
+        List<Tuple<Tuple<int, int>, string>> roomDayBlock = timeBlockings[room][day];
+        if (roomDayBlock.Count == 0) {
+            Console.WriteLine("The room is availabe from {0} to {1}", earliest, lateset);
+            result.Add(new Tuple<int, int>(earliest, lateset));
+            return result;
+        }
+        else
+        {
+            // Sort the roomDayBlock by start times . . . unsure how to do this . . .
+            // Calculate the availability
+        }
+        return result;
+    }
+
+
+    static public Dictionary<string, Dictionary<string, List<Tuple<Tuple<int, int>, string>>>> processWeekdayTimeBlockings(Dictionary<string, List<Tuple<string, Tuple<int, int>, string, string, string, string, string>>> coursesRev)
+    {
+        Dictionary<string, Dictionary<string, List<Tuple<Tuple<int, int>, string>>>> result = new Dictionary<string, Dictionary<string, List<Tuple<Tuple<int, int>, string>>>>();
+        foreach (var item in coursesRev)
+        {
+            //Console.WriteLine(item.Key);
+            foreach (var lecture in item.Value)
+            {
+                string room = lecture.Item1;
+                Tuple<int, int> time = lecture.Item2;
+                string cid = lecture.Item3;
+                string weekday = lecture.Item4;
+                if (room != "NULL" & weekday != "NULL")
+                {
+                    //Console.WriteLine("Trying to add the room to the reuslt");
+                    if (!result.ContainsKey(room))
+                    {
+                        result.Add(room, new Dictionary<string, List<Tuple<Tuple<int, int>, string>>>(){
+                    {"Monday", new List<Tuple<Tuple<int, int>, string>>()},
+                    {"Tuesday", new List<Tuple<Tuple<int, int>, string>>()},
+                    {"Wednesday", new List<Tuple<Tuple<int, int>, string>>()},
+                    {"Thursday", new List<Tuple<Tuple<int, int>, string>>()},
+                    {"Friday", new List<Tuple<Tuple<int, int>, string>>()},
+                    {"Saturday", new List<Tuple<Tuple<int, int>, string>>()},
+                    {"Sunday", new List<Tuple<Tuple<int, int>, string>>()}
+                });
+                    }
+                    //Console.WriteLine("Room was successfulyl added . . .");
+                    // Need to add a room, value pair to result . . .
+                    if (weekday.IndexOf(',') != -1)
+                    {
+                        //Console.WriteLine("Many Week Days . . .");
+                        string[] weekdays = weekday.Split(',');
+                        foreach (string day in weekdays)
+                        {
+                            //Console.WriteLine(day);
+                            result[room][day].Add(new Tuple<Tuple<int, int>, string>(time, cid));
+                        }
+                    }
+                    else
+                    {
+                        //Console.WriteLine("There is one day . . .");
+                        //Console.WriteLine(weekday);
+                        result[room][weekday].Add(new Tuple<Tuple<int, int>, string>(time, cid));
+                    }
+                }
+            }
+        }
+        /*foreach (var item in result)
+        {
+            Console.WriteLine(item.Key);
+            foreach (var day in item.Value)
+            {
+                Console.WriteLine("\t" + day.Key);
+                foreach (var item2 in day.Value)
+                {
+                    Console.WriteLine("\t\t" + item2.Item1 + " " + item2.Item2);
+                }
+            }
+        }*/
+        return result;
+    }
+
+
+    static public string weekdayConverter(string shorthand)
+    {
+        if (shorthand.Length == 1)
+        {
             return dayMappings[shorthand];
         }
-        if (shorthand.Length == 2) {
-            if (Char.IsUpper(shorthand[1])) {
-                return dayMappings[shorthand[0].ToString()] + ", " + dayMappings[shorthand[1].ToString()];
-            } else {
+        if (shorthand.Length == 2)
+        {
+            if (Char.IsUpper(shorthand[1]))
+            {
+                return dayMappings[shorthand[0].ToString()] + "," + dayMappings[shorthand[1].ToString()];
+            }
+            else
+            {
                 return dayMappings[shorthand];
             }
         }
-        if (shorthand.Length == 3) {
-            return dayMappings[shorthand[0].ToString()] + ", " + dayMappings[shorthand.Substring(1)];
+        if (shorthand.Length == 3)
+        {
+            return dayMappings[shorthand[0].ToString()] + "," + dayMappings[shorthand.Substring(1)];
         }
-        if (shorthand.Length == 4) {
-            if (Char.IsUpper(shorthand[2])) {
-                return dayMappings[shorthand[0].ToString()] + ", " + dayMappings[shorthand.Substring(1,3)] + ", " + dayMappings[shorthand[3].ToString()];
+        if (shorthand.Length == 4)
+        {
+            if (Char.IsUpper(shorthand[2]))
+            {
+                return dayMappings[shorthand[0].ToString()] + "," + dayMappings[shorthand.Substring(1, 3)] + "," + dayMappings[shorthand[3].ToString()];
             }
         }
         return "NULL";
     }
 
 
-    static public int convertToMilitary(string stdTime) {
+    static public int convertToMilitary(string stdTime)
+    {
         int stdTimeLength = stdTime.Length;
         string amOrPm = stdTime.Substring(stdTimeLength - 2);
-        if (amOrPm == "am") {
-            if (stdTimeLength == 3) {
+        if (amOrPm == "am")
+        {
+            if (stdTimeLength == 3)
+            {
                 return Int32.Parse("0" + stdTime[0].ToString() + "00");
-            } else if (stdTimeLength == 4) {
-                Console.WriteLine(Int32.Parse(stdTime.Substring(0,2)));
+            }
+            else if (stdTimeLength == 4)
+            {
+                //Console.WriteLine(Int32.Parse(stdTime.Substring(0, 2)));
                 return Int32.Parse(stdTime.Substring(0, 2) + "00");
-            } else if (stdTimeLength == 6) {
+            }
+            else if (stdTimeLength == 6)
+            {
                 return Int32.Parse("0" + stdTime[0].ToString() + stdTime.Substring(2, 2));
-            } else if (stdTimeLength == 7) {
+            }
+            else if (stdTimeLength == 7)
+            {
                 return Int32.Parse(stdTime.Substring(0, 2) + stdTime.Substring(3, 2));
             }
-        } else if (amOrPm == "pm") {
-            if (stdTimeLength == 3) {
+        }
+        else if (amOrPm == "pm")
+        {
+            if (stdTimeLength == 3)
+            {
                 return Int32.Parse((12 + Int32.Parse(stdTime[0].ToString())).ToString() + "00");
-            } else if (stdTimeLength == 4) {
+            }
+            else if (stdTimeLength == 4)
+            {
                 return Int32.Parse((12 + Int32.Parse(stdTime.Substring(0, 2))).ToString() + "00");
-            } else if (stdTimeLength == 6) {
+            }
+            else if (stdTimeLength == 6)
+            {
                 return Int32.Parse((12 + Int32.Parse(stdTime[0].ToString())).ToString() + stdTime.Substring(2, 2));
-            } else if (stdTimeLength == 7) {
+            }
+            else if (stdTimeLength == 7)
+            {
                 return Int32.Parse((12 + Int32.Parse(stdTime.Substring(0, 2))).ToString() + stdTime.Substring(3, 2));
             }
-        } else {
-            Console.WriteLine("convertToMilitary(): ERROR: Hmm I am unsure what to do with" + stdTime +". . .");
+        }
+        else
+        {
+            Console.WriteLine("convertToMilitary(): ERROR: Hmm I am unsure what to do with" + stdTime + ". . .");
         }
 
-        
+
         return -1;
     }
 
@@ -94,7 +215,7 @@ class CVSParser
                 }
                 else if (cross & rowCount > 2 & row.Length < 50)
                 {
-                    Console.WriteLine(row);
+                    //Console.WriteLine(row);
                     int x = 0;
                     // Find a way to apply this cross list to the previously processed line
                 }
@@ -117,33 +238,39 @@ class CVSParser
     }
 
 
-    static public int validateMilitaryTime(int militaryTime) {
-        if (militaryTime == -1) {
+    static public int validateMilitaryTime(int militaryTime)
+    {
+        if (militaryTime == -1)
+        {
             return -1;
         }
         int hour = militaryTime / 100;
         int minute = militaryTime % 100;
-        if (!(minute <= 0) & !(minute >= 50)) {
+        if (!(0 <= minute & minute <= 59))
+        {
             hour += 1;
             minute -= 60;
         }
 
-        if (hour > 24) {
+        if (hour > 24)
+        {
             hour = 0;
         }
-        if (hour == 24 & minute > 0) {
+        if (hour == 24 & minute > 0)
+        {
             hour = 0;
         }
-        return Int32.Parse(hour.ToString() + minute.ToString().PadRight(2));
+        return Int32.Parse(hour.ToString() + minute.ToString().PadRight(2, '0'));
     }
 
-    static public Dictionary<string, List<string[]>> process(Dictionary<string, List<string[]>> inData)
+
+    static public Dictionary<string, List<Tuple<string, Tuple<int, int>, string, string, string, string, string>>> process(Dictionary<string, List<string[]>> inData)
     {
-        Dictionary<string, List<string[]>> result = new Dictionary<string, List<string[]>>();
+        Dictionary<string, List<Tuple<string, Tuple<int, int>, string, string, string, string, string>>> result = new Dictionary<string, List<Tuple<string, Tuple<int, int>, string, string, string, string, string>>>();
         foreach (var item in inData)
         {
             string course = item.Key;
-            result.Add(course, new List<string[]>());
+            result.Add(course, new List<Tuple<string, Tuple<int, int>, string, string, string, string, string>>());
             foreach (string[] lecture in item.Value)
             {
                 if (lecture.Length != 0)
@@ -151,9 +278,12 @@ class CVSParser
                     string CID = course.Substring(0, 9) + "-" + lecture[9].PadLeft(3, '0');
                     string SID = lecture[3];
                     string ROOM = lecture[16];
+                    Match s = Regex.Match(ROOM, @"Peter Kiewit Institute");
+                    if (s.Success) {
+                        ROOM = "PKI " + ROOM.Substring(ROOM.Length - 3, 3);
+                    }
                     string CROSS = "N";
                     string CROSSID = "NULL";
-                    Match s;
                     if (lecture[35] != "")
                     {
                         Regex.Replace(lecture[35], "\n", " ");
@@ -177,7 +307,7 @@ class CVSParser
                         }
                     }
                     string TIMEWEEK = lecture[13];
-                    string TIME = "NULL";
+                    Tuple<int, int> TIME = new Tuple<int, int>( -1, -1 );
                     string WEEKDAY = "NULL";
                     s = Regex.Match(TIMEWEEK, @"Does Not Meet");
                     if (!s.Success)
@@ -185,11 +315,7 @@ class CVSParser
                         //Console.WriteLine("TIMEWEEK: " + TIMEWEEK);
                         string[] TIMEWEEKSplit = TIMEWEEK.Split(' ');
                         string[] hourMinute = TIMEWEEKSplit[1].Split('-');
-                        TIME = validateMilitaryTime(convertToMilitary(hourMinute[0])).ToString() + "-" + validateMilitaryTime(convertToMilitary(hourMinute[1])).ToString();
-                        if (TIME == "-1--1") {
-                            TIME = "NULL";
-                        }
-                        Console.WriteLine("TIME: " + TIME);
+                        TIME = new Tuple<int, int>(validateMilitaryTime(convertToMilitary(hourMinute[0])),validateMilitaryTime(convertToMilitary(hourMinute[1])));
                         //Console.WriteLine("TIMEWEEKSplit Length: " + TIMEWEEKSplit.Length);
                         WEEKDAY = weekdayConverter(TIMEWEEKSplit[0]);
                         // Will need to implement convert to military . . . 
@@ -198,7 +324,7 @@ class CVSParser
                     {
                         ROOM = "NULL";
                     }
-                    result[course].Add(new string[] { ROOM, TIME, CID, WEEKDAY, CROSS, CROSSID, SID });
+                    result[course].Add(new Tuple<string, Tuple<int,int>, string, string, string, string, string>( ROOM, TIME, CID, WEEKDAY, CROSS, CROSSID, SID ));
                     /*foreach (string[] s1 in result[course])
                     {
                         for (int k = 0; k < s1.Length; k++)
@@ -228,18 +354,23 @@ class CVSParser
         // List<string> csvfiles = new List<string> { "CSCI1191.csv", "CYBR1191.csv" };
         List<string> csvfiles = new List<string> { "CSCI1191.csv", "BIOI1191.csv", "BMI1191.csv", "CYBR1191.csv", "ISQA1191.csv", "ITIN1191.csv" };
         Dictionary<string, List<string[]>> baseData = processBaseFiles(csvfiles);
-        /*foreach( var item in baseData) {
-            Console.WriteLine(item.Key);
-            foreach (string[] s in item.Value) {
-                for(int i = 0; i < s.Length; i++) {
-                    if (s[i] != string.Empty)
-                    {
-                        Console.Write(s[i] + "  ");
-                    }
-                }
-                Console.WriteLine();
+        Dictionary<string, List<Tuple<string, Tuple<int, int>, string, string, string, string, string>>> coursesRevised = process(baseData);
+        Dictionary<string, Dictionary<string, List<Tuple<Tuple<int, int>, string>>>> weekDayTimeBlockings = processWeekdayTimeBlockings(coursesRevised);
+        Console.WriteLine("Enter a Room: ");
+        string room = Console.ReadLine();
+        if (room == null) {
+            room = "PKI 361";
+        }
+        Console.WriteLine("Enter a Day: ");
+        string day = Console.ReadLine();
+        if (day == null) {
+            day = "Tuesday";
+        }
+        List<Tuple<int, int>> available = getAvailability(weekDayTimeBlockings, room, day);
+        if (available != null) {
+            foreach(var item in available) {
+                Console.WriteLine(item);
             }
-        }*/
-        Dictionary<string, List<string[]>> coursesRevised = process(baseData);
+        }
     }
 }
