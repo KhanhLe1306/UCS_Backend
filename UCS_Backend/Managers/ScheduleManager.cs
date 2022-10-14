f306c26a021b8799c766d97c16a7e17b33446e62
Khanh Le <khanhle@unomaha.edu>
2022-10-13 22:57:59 -0500

    UCS_31: Read and Add Class to DB

---
+++ UCS_Backend/Managers/ScheduleManager.cs
@@ -0,0 +1,19 @@
+﻿using UCS_Backend.Interfaces;
+using UCS_Backend.Interfaces.IManagers;
+using UCS_Backend.Models;
+
+namespace UCS_Backend.Managers
+{
+    public class ScheduleManager : IScheduleManager
+    {
+        private IScheduleRepository _scheduleRepository;
+        public ScheduleManager()
+        {
+
+        }
+        public Schedule AddSchedule(ScheduleFormBody body)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
