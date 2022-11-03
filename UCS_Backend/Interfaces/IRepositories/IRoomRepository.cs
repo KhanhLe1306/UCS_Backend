536759bb2fd73b6c521b62d7052d1e4346e47a3c
Khanh Le <khanhle@unomaha.edu>
2022-11-02 22:14:33 -0500

    Search by RoomNumber

--- UCS_Backend/Interfaces/IRepositories/IRoomRepository.cs
+++ UCS_Backend/Interfaces/IRepositories/IRoomRepository.cs
@@ -1,9 +1,12 @@
 ﻿using UCS_Backend.Models;
+using UCS_Backend.Models.SubModels;
 
 namespace UCS_Backend.Interfaces.IRepositories
 {
     public interface IRoomRepository: IBaseRepository<Room>
     {
         int FindRoomIdByName(string name);
+
+        List<ScheduleInfo> GetScheduleByRoomNumber(int roomNumber);
     }
 }
---
+++ UCS_Backend/Models/SubModels/ScheduleInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace UCS_Backend.Models.SubModels
+{
+    public class ScheduleInfo
+    {
+        public string RoomName { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+        public string Course { get; set; }
+        public string CourseTitle { get; set; }
+        public string MeetingDays { get; set; }
+    }
+}
