4f45fa4df43ae07e2d236ba05878852dab5dafc0
Ethan Christensen <55725575+ethanlchristensen@users.noreply.github.com>
2022-11-03 22:39:22 -0500

    DataContext

--- UCS_Backend/Data/DataContext.cs
+++ UCS_Backend/Data/DataContext.cs
@@ -17,6 +17,7 @@ namespace UCS_Backend.Data
         public virtual DbSet<Weekday> Weekdays { get; set; }
         public virtual DbSet<Schedule> Schedules { get; set; }
         public virtual DbSet<Cross> Cross { get; set; }
+        public virtual DbSet<Instructor> Instructors { get; set; }
     }
 }
 
