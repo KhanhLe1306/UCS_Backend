5db35d487ed3a55af924c5876b5a4a080e1cb355
Khanh Le <khanhle@unomaha.edu>
2022-11-29 16:26:33 -0600

    HttpPost body from Frontend

--- UCS_Backend/Models/ClassModel.cs
+++ UCS_Backend/Models/ClassModel.cs
@@ -14,4 +14,16 @@ namespace UCS_Backend.Models
         public string CatalogNumber { get; set; }
 
     }
+
+    public class AddClassModel
+    {
+        public string Cls { get; set; }
+        public string Section { get; set; }
+        public string Instructor { get; set; }
+        public string ClassSize { get; set; }
+        public string ClassTime { get; set; }
+        public string RoomCode { get; set; }
+        public string Room { get; set; }
+        public string Days { get; set; }
+    }
 }
